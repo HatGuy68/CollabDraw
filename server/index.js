6f3f5edbc7b771f1556a36a3b5c4e644d156e771
HatGuy68 <irahul.2k@gmail.com>
2021-12-31 01:07:15 +0530

    files moved to server folder; cors pass added

---
+++ server/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const httpServer = require("http").createServer(app);
+const io = require("socket.io")(httpServer);
+const cors = require('cors');
+
+app.use(cors());
+
+connections = [];
+
+io.on('connect', (socket) => {
+    connections.push(socket);
+    console.log(`${socket.id} has connected`);
+
+    socket.on("propogate", (data) => {
+        connections.map((con) => {
+            if (con.id !== socket.id) {
+                con.emit("onpropogate", data);
+            }
+        });
+    });
+
+    socket.on("down", (data) => {
+        connections.map((con) => {
+            if (con.id !== socket.id) {
+                con.emit("ondown", data);
+            }
+        });
+    });
+
+    socket.on('disconnect', (reason) => {
+        console.log(`${socket.id} has disconnected`);
+        connections = connections.filter(conn => conn.id !== socket.id)
+    })
+})
+
+app.use(express.static("./../public"));
+
+let PORT = process.env.PORT || 8080;
+httpServer.listen(PORT, () => console.log(`Server started on port ${PORT}`));
