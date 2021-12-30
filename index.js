50f15319f4b1d67d39107c89ac3291321b175e4b
HatGuy68 <irahul.2k@gmail.com>
2021-12-30 13:51:25 +0530

    trial for access control

--- index.js
+++ index.js
@@ -32,6 +32,13 @@ io.on('connect', (socket) => {
 })
 
 app.use(express.static("public"));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 let PORT = process.env.PORT || 8080;
 httpServer.listen(PORT, () => console.log(`Server started on port ${PORT}`));
